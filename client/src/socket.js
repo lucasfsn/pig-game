a4328fdda8a11a167b68afcd1e13700e15d4d91e
lukasz <luki424221@gmail.com>
2024-01-05 16:06:41 +0100

    :fire: Remove unused files

--- client/src/socket.js
+++
@@ -1,7 +0,0 @@
-import io from 'socket.io-client';
-
-const apiUrl = process.env.REACT_APP_BACKEND_API_URL;
-
-const socket = io.connect(apiUrl);
-
-export default socket;
--- server/src/routes/chat.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import { Router } from 'express';
-import * as ChatController from '../controllers/chat.js';
-
-export const chatRouter = Router().get(
-  '/:gameId',
-  ChatController.getChatHistory
-);
