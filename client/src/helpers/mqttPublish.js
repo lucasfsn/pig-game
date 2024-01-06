3f2f727bcbb8bb04904938f011b487f96b5789b3
lukasz <luki424221@gmail.com>
2024-01-06 21:54:59 +0100

    :art: Update MQTT connection to use environment variable

--- client/src/helpers/mqttPublish.js
+++ client/src/helpers/mqttPublish.js
@@ -1,7 +1,9 @@
 import mqtt from 'mqtt';
 
+const brokerUrl = process.env.REACT_APP_BROKER_URL;
+
 export function mqttPublish(topic, message) {
-  const client = mqtt.connect('ws://localhost:9001/mqtt');
+  const client = mqtt.connect(brokerUrl);
 
   client.on('connect', () => {
     client.publish(topic, message);
--- client/src/hooks/useMqttSubscribe.js
+++ client/src/hooks/useMqttSubscribe.js
@@ -1,11 +1,13 @@
 import mqtt from 'mqtt';
 import { useEffect, useState } from 'react';
 
+const brokerUrl = process.env.REACT_APP_BROKER_URL;
+
 export function useMqttSubscribe(topic) {
   const [message, setMessage] = useState(null);
 
   useEffect(() => {
-    const client = mqtt.connect('ws://localhost:9001/mqtt');
+    const client = mqtt.connect(brokerUrl);
 
     client.on('connect', () => {
       client.subscribe(topic);
